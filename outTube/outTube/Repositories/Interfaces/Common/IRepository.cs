7724c222bcf72368c2c90e138ebe39fa8f910b01
khaledahmed <khaled.ahmed333774@gmail.com>
2026-03-25 01:34:20 +0200

    add generic repo interface ^_^

---
+++ outTube/outTube/Repositories/Interfaces/Common/IRepository.cs
@@ -0,0 +1,14 @@
+﻿using System.Linq.Expressions;
+
+namespace OurTube.Repositories.Interfaces.Common
+{
+    public interface IRepository<T>
+    {
+        public List<T> GetByCondition(Expression<Func<T, bool>> predicate);
+        public List<T> GetAll();
+        public bool Create(T entity);
+        public bool Update(T entity);
+        public bool Delete(T entity);
+        public int Save();
+    }
+}
