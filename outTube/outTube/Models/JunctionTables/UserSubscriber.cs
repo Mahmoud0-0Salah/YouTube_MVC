d89b244bbf66a20f88215b4e02f50fbfff3f76d1
Elaraby218 <152742814+Elaraby218@users.noreply.github.com>
2026-03-25 22:39:50 +0200

    fix some errors and add some validations

--- outTube/outTube/Models/JunctionTables/UserSubscriber.cs
+++ outTube/outTube/Models/JunctionTables/UserSubscriber.cs
@@ -2,26 +2,33 @@ using outTube.Models;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
-namespace outTube.Models.JunctionTables;
-
-public class UserSubscriber
+public class UserSubscriber : IValidatableObject
 {
     [Key]
     public int Id { get; set; }
 
-    [Required]
     [MaxLength(450)]
     public string UserId { get; set; }
 
     [ForeignKey("UserId")]
     public User User { get; set; }
 
-    [Required]
     [MaxLength(450)]
+    [NotSelfReference("UserId")]
     public string SubscriberId { get; set; }
 
     [ForeignKey("SubscriberId")]
     public User Subscriber { get; set; }
 
     public DateTime SubscribedAt { get; set; } = DateTime.UtcNow;
+
+    public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+    {
+        if (UserId == SubscriberId)
+        {
+            yield return new ValidationResult(
+                "A user cannot subscribe to themselves.",
+                new[] { nameof(SubscriberId) });
+        }
+    }
 }
