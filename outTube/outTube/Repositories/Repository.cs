b96d1cacca08de1d68bb1f74fbd84a62fcde327c
khaledahmed <khaled.ahmed333774@gmail.com>
2026-03-25 02:04:25 +0200

    add generic repo with  generic implementation ^_^

---
+++ outTube/outTube/Repositories/Repository.cs
@@ -0,0 +1,97 @@
+﻿using OurTube.Data;
+using OurTube.Models;
+using OurTube.Repositories.Interfaces.Common;
+using System.Linq.Expressions;
+
+namespace OurTube.Repositories
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        private readonly OurTubeContext context;
+
+        public Repository(OurTubeContext context)
+        {
+            this.context = context;
+        }
+        public bool Create(T entity)
+        {
+            try
+            {
+                context.Set<T>().Add(entity);
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return false;
+            }
+        }
+
+        public bool Delete(T entity)
+        {
+            try
+            {
+                context.Set<T>().Remove(entity);
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return false;
+            }
+        }
+
+        public List<T> GetAll()
+        {
+            try
+            {
+                return context.Set<T>().ToList();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return new List<T>();
+            }
+        }
+
+        public List<T> GetByCondition(Expression<Func<T, bool>> predicate)
+        {
+            try
+            {
+                return context.Set<T>().Where(predicate).ToList();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return new List<T>();
+            }
+        }
+
+        public int Save()
+        {
+            try
+            {
+                return context.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return -1;
+            }
+        }
+
+        public bool Update(T entity)
+        {
+            try
+            {
+                context.Set<T>().Update(entity);
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return false;
+            }
+        }
+    }
+}
