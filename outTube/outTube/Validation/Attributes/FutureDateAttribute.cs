44192a9cc863dfa3ce34d46d87cac1156e001a08
Elaraby218 <152742814+Elaraby218@users.noreply.github.com>
2026-03-25 22:28:56 +0200

    Custom validation attributes

--- outTube/outTube/Validation/Attributes/FutureDateAttribute.cs
+++ outTube/outTube/Validation/Attributes/FutureDateAttribute.cs
@@ -1,6 +1,20 @@
-﻿namespace outTube.Validation.Attributes
+﻿using System.ComponentModel.DataAnnotations;
+
+public class FutureDateAttribute : ValidationAttribute
 {
-    public class FutureDateAttribute
+    public FutureDateAttribute()
     {
+        ErrorMessage = "Date cannot be in the future.";
+    }
+
+    protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+    {
+        if (value is DateTime date)
+        {
+            if (date.Date > DateTime.Today)
+                return new ValidationResult(ErrorMessage);
+        }
+
+        return ValidationResult.Success;
     }
 }
--- outTube/outTube/Validation/Attributes/MinimumAgeAttribute.cs
+++ outTube/outTube/Validation/Attributes/MinimumAgeAttribute.cs
@@ -1,6 +1,29 @@
-﻿namespace outTube.Validation.Attributes
+﻿using System.ComponentModel.DataAnnotations;
+
+public class MinimumAgeAttribute : ValidationAttribute
 {
-    public class MinimumAgeAttribute
+    private readonly int _minimumAge;
+
+    public MinimumAgeAttribute(int minimumAge)
     {
+        _minimumAge = minimumAge;
+        ErrorMessage = $"You must be at least {_minimumAge} years old.";
+    }
+
+    protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+    {
+        if (value is DateTime dateOfBirth)
+        {
+            var today = DateTime.Today;
+            var age = today.Year - dateOfBirth.Year;
+
+            if (dateOfBirth.Date > today.AddYears(-age))
+                age--;
+
+            if (age < _minimumAge)
+                return new ValidationResult(ErrorMessage);
+        }
+
+        return ValidationResult.Success;
     }
 }
