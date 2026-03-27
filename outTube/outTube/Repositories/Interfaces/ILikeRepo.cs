7e68f671580da1381559854f42bd400c77c9ef75
Mahmoud0-0Salah <mahmoudsalah2339@gmail.com>
2026-03-27 04:00:05 +0200

    Add liked videos page

--- outTube/outTube/Repositories/Interfaces/ILikeRepo.cs
+++ outTube/outTube/Repositories/Interfaces/ILikeRepo.cs
@@ -1,9 +1,12 @@
-﻿using OurTube.Repositories.Interfaces.Common;
+﻿using ourTube.ViewModels;
+using ourTube.ViewModels.Video;
+using OurTube.Repositories.Interfaces.Common;
 using outTube.Models.JunctionTables;
 
 namespace ourTube.Repositories.Interfaces
 {
 	public interface ILikeRepo:IRepository<LikeVideo>
 	{
+		PaginatedList<VideoGetViewModel> GetLikedVideosInfo(string useerId, int page = 1, int pageSize = 8);
 	}
 }
--- outTube/outTube/Repositories/LikeRepo.cs
+++ outTube/outTube/Repositories/LikeRepo.cs
@@ -1,5 +1,9 @@
-﻿using OurTube.Repositories;
+﻿using Microsoft.EntityFrameworkCore;
+using ourTube.ViewModels;
+using ourTube.ViewModels.Video;
+using OurTube.Repositories;
 using outTube.Data;
+using outTube.Models;
 using outTube.Models.JunctionTables;
 
 namespace ourTube.Repositories.Interfaces
@@ -10,6 +14,39 @@ namespace ourTube.Repositories.Interfaces
 		{
 		}
 
+		private static VideoGetViewModel GetVideoGetViewModel(Video video)
+		{
+			return new VideoGetViewModel
+			{
+				Id = video.VideoId,
+				Title = video.Title,
+				Channel = video.User.FirstName + " " + video.User.LastName,
+				Description = video.Description ?? string.Empty,
+				Duration = video.Duration.ToString(@"hh\:mm\:ss"),
+				Time = video.CreatedAt.ToString("MMM dd, yyyy"),
+				Thumb = video.ThumbnailUrl,
+				Avatar = video.User.ImageUrl,
+				VideoUrl = video.VideoUrl,
+				Views = video.Views.Count
+			};
+		}
+
+		public PaginatedList<VideoGetViewModel> GetLikedVideosInfo(string useerId, int page = 1, int pageSize = 8)
+		{
+			List<VideoGetViewModel> items = GetByCondition(l=>l.UserId == useerId)
+			   .Include(l=>l.Video)
+			   .ThenInclude(v=>v.User)
+			   .Where(Video => Video.Video.Visible)
+			   .Select(l => GetVideoGetViewModel(l.Video))
+			   .Skip((page - 1) * pageSize)
+			   .Take(pageSize)
+			   .ToList();
+
+			return new PaginatedList<VideoGetViewModel>(items, GetByCondition(l=>l.UserId==useerId)
+				.Include(l=>l.Video)
+				.Where(l=>l.Video.Visible)
+				.Count(), page, pageSize);
+		}
 
 	}
 }
